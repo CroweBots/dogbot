8bbcbfdeb9cd1c30fb3340e005fb07a24a92e3da
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-09-27 15:48:12 -0700

    added aliases

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 var HTTPS = require('https');
-
+var FS = require('fs')
 //Load all the config variables
 var botID = process.env.BOT_ID;
 var name1 = process.env.name;
@@ -12,13 +12,106 @@ function(str){
     return this.indexOf(str)!= -1;
 };
 
+aliases = load_aliases();
+
+function load_aliases() {
+    members = 
+
+    // WRITE THIS FUNCTION
+}
+
+function setAlias(nickname, alias) {
+    var options, body, botReq;
+
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/groups/8717323',
+        method: 'GET'
+    };
+    //if response is long, replace the 1000th character with an elipsis and send it
+    body = {};
+    
+    botReq = HTTPS.request(options, function(res) {
+        if(res.statusCode == 200) {
+            res.on('data', function(data) {
+                var aliases = JSON.parse(fs.readFile("aliases.json"));
+                postMessage(nickname + " is now " + alias + "! ;)");
+                
+                var user_id;
+                var valid_user = false;
+                for (var i = 0; i < data["members"].length; i++) {
+                    if (nickname == data["members"][i]["nickname"]) {
+                        user_id = data["members"][i]["user_id"];
+                        valid_user = true;
+                    }
+                }
+                if (valid_user) {
+                    aliases[alias] = user_id;
+                }
+
+                var new_aliases = JSON.stringify(object)
+                fs.writeFile("aliases.json", new_aliases);
+            });
+        } else {
+            postMessage("Could not set alias of " + nickname + " to " + alias + ". Try again! :(");
+        }
+    });
+}
+
 
 function respond() {
     var request = JSON.parse(this.req.chunks[0]);
     var message = "";
     //default responses for the dogbot
     var barks = ["woof", "woof woof", "ruff", "ruff ruff", "arf", "arf arf", "bow wow", "yap", "yap yap", "yip", "yip yip", "wuff", "wuff wuff", "wau", "wau wau", "hev", "hev hev", "hav", "hav hav", "guau-guau", "gua", "gua gua","jau","jau jau", "blaf", "blaf blaf", "woef", "woef woef", "keff", "keff keff", "gav", "gav gav", "tyav", "tyav tyav", "meong", "meong meong", "wan", "wan-wan", "kyan", "kyan-kyan", "bau", "bau bau", "bow", "bow bow", "voff", "voff voff", "blaf", "blaf blaf", "kef", "kef kef", "waf", "waf waf", "woef", "woef woef", "vov", "vuf", "wang", "wang wang", "ham", "ham ham", "waouh", "waouh waouh", "ouah", "ouah ouah", "ouaf", "ouaf ouaf", "vaf", "vaf vaf", "wouf", "wouf wouf", "wouaf", "wouaf wouaf", "jappe", "jappe jappe", "💩", "💩💩", "💩💩💩", "💩💩💩💩", "💩💩💩💩💩", "💩💩💩💩💩💩", "💩💩💩💩💩💩💩", "😀", "😁", "😂", "😃", "😄", "😅", "😆", "😇", "😉", "😊", "☺️", "😋", "😌", "😍", "😎", "😏", "😐", "😑", "😒", "😓", "😔", "😕", "😖", "😗", "😘", "😚", "😛", "😜", "😝", "😞", "😟", "😠", "😡", "😢", "😣", "😤", "😈", "👿", "😥", "😦", "😧", "😨", "😩", "😪", "😫", "😬", "😭", "😮", "😯", "😰", "😱", "😲", "😳", "😴", "😴😴😴", "😴😴", "😵", "😶", "😷", "👣", "👣👣", "👣👣👣", "👣👣👣👣", "👏", "👏👏", "👏👏👏", "👅👅👅👅👅", "👅👅👅👅", "👅👅👅", "👅👅", "👅", "👍", "👎", "👌", "🙏", "🐶", "💦🌱", "💦🌾", "💦🌲", "💦🌳", "💦🌴", "💦🌵", "💦🌵🌵", "💦🌵🌵🌵", "💦🌵🌵🌵🌵", "💦🌵🌵🌵🌵🌵", "🍖", "🍗", "🍚", "🍼", "🎾", "⚽️", "🏀", "🏈", "⚾️", "💤", "📬"];
+    var aliases = JSON.parse(fs.readFile("aliases.json"));
+
+    // sets alias
+    if (request.text.toLowerCase().contains("set alias")) {
+        var components = request.text.split(" ");
+        var nickname = "";
+        var alias = "";
+        for (var i = 0; i < components.length - 2; i++) {
+            var component = components[i];
+            if (component == "alias") {
+                nickname = components[i+1];
+                alias = components[i+2];
+            }
+        }
+
+        if (alias != "") {
+            setAlias(nickname, alias);
+        }
+    }
     
+    // checks if message contains alias
+    var contains_alias = false;
+    for (var alias in aliases) {
+        if request.text.contains(alias) {
+           contains_alias = true;
+        }
+    }
+    // substitutes all aliases in
+    if (contains_alias) {
+        var message = request.text;
+        var attachments = {};
+        attachments["loci"] = [];
+        attachments["type"] = "mentions";
+        attachments["user_ids"] = []; 
+        for (var alias in aliases) {
+            if request.text.contains(alias) {
+                this.res.writeHead(200);
+
+                var indices = {message.indexOf(alias), alias.length};
+                attachments["loci"].push(indices);
+                attachments["user_ids"].push(aliases[alias]);
+                message = message.replace(alias, "@" + aliases[alias]);
+            }
+        }
+
+        postMessageWithAttachments(message, attachments);
+    }
+
     if((request.text.contains(name1)|| request.text.contains(name2) )&& request.text.contains("fetch")) {
         //if the user types the bot's name and fetch, fetch RSS feeds they want
         //Currently supports headlines from Any Subreddit, Al Jazeera, and QZ
@@ -100,6 +193,41 @@ function choose(text){
 function random(num){
     return Math.floor((Math.random() * num) + 1);
 }
+
+// sorry DRY but I was lazy
+function postMessageWithAttachments(response, attachments) {
+    var options, body, botReq;
+    
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+    };
+    //if response is long, replace the 1000th character with an elipsis and send it
+    response = response.length > 1000 ? response.substring(0,1000-1)+'…' : response;
+    body = {
+        "bot_id" : botID,
+        "text" : response,
+        "attachments": attachments,
+    };
+    
+    botReq = HTTPS.request(options, function(res) {
+                           if(res.statusCode == 202) {
+                           //message has been sent
+                           } else {
+                           console.log('rejecting bad status code ' + res.statusCode);
+                           }
+                           });
+    
+    botReq.on('error', function(err) {
+              console.log('error posting message '  + JSON.stringify(err));
+              });
+    botReq.on('timeout', function(err) {
+              console.log('timeout posting message '  + JSON.stringify(err));
+              });
+    botReq.end(JSON.stringify(body));
+}
+
 
 //post message to the group
 function postMessage(response) {
